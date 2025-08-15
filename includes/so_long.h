6916d211f510abc9a480a79e95a15c55b9cffed0
AdnilsonPinheiro <adpmaia@gmail.com>
2025-08-15 18:33:12 +0100

    home

--- includes/so_long.h
+++ includes/so_long.h
@@ -6,7 +6,7 @@
 /*   By: adpinhei <adpinhei@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/11 20:13:54 by adpinhei          #+#    #+#             */
-/*   Updated: 2025/08/14 18:11:27 by adpinhei         ###   ########.fr       */
+/*   Updated: 2025/08/15 17:21:00 by adpinhei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@
 # include "../mlx_linux/mlx.h"
 # include <stdlib.h>
 # include <X11/keysym.h>
+# include <X11/X.h>
 
 # ifndef WIDTH
 #  define WIDTH 1000
@@ -30,5 +31,14 @@ typedef struct s_data
 	void	*mlx_ptr;
 	void	*win_ptr;
 }	t_data;
+
+typedef struct s_rect
+{
+	int	x;
+	int	y;
+	int	width;
+	int	height;
+	int	color;
+}	t_rect;
 
 #endif
--- src/main.c
+++ src/main.c
@@ -6,15 +6,20 @@
 /*   By: adpinhei <adpinhei@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/12 17:06:32 by adpinhei          #+#    #+#             */
-/*   Updated: 2025/08/14 18:08:46 by adpinhei         ###   ########.fr       */
+/*   Updated: 2025/08/15 17:28:04 by adpinhei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 #define MLX_ERROR 1
+#define RED_PIXEL 0xFF0000
+#define GREEN_PXL 0xFF00
+#define WHITE_PIXEL 0xFFFFFF
 
-static int	handle_no_event(void *data);
-static int	handle_input(int keysym, t_data *data);
+static int	handle_keypress(int keysym, t_data *data);
+static int	render(t_data *data);
+static int	render_rect(t_data *data, t_rect rect);
+static void	render_backgrd(t_data *data, int color);
 
 int	main(void)
 {
@@ -29,23 +34,65 @@ int	main(void)
 		free(data.mlx_ptr);
 		return (MLX_ERROR);
 	}
-	mlx_loop_hook(data.mlx_ptr, &handle_no_event, &data);
-	mlx_key_hook(data.win_ptr, &handle_input, &data);
-
+	mlx_loop_hook(data.mlx_ptr, &render, &data);
+	mlx_hook(data.win_ptr, KeyPress, KeyPressMask, &handle_keypress, &data);
 	mlx_loop(data.mlx_ptr);
 	mlx_destroy_display(data.mlx_ptr);
 	free(data.mlx_ptr);
 }
 
-static int	handle_no_event(void *data)
+static int	handle_keypress(int keysym, t_data *data)
 {
-	(void)data;
+	if (keysym == XK_Escape)
+	{
+		mlx_destroy_window(data->mlx_ptr, data->win_ptr);
+		data->win_ptr = NULL;
+	}
 	return (0);
 }
 
-static int	handle_input(int keysym, t_data *data)
+static int	render(t_data *data)
 {
-	if (keysym == XK_Escape)
-		mlx_destroy_window(data->mlx_ptr, data->win_ptr);
+	if (data->win_ptr)
+	{
+		render_backgrd(data, WHITE_PIXEL);
+		render_rect(data, (t_rect){WIDTH-100, HEIGHT-100, 100, 100, GREEN_PXL});
+		render_rect(data, (t_rect){0, 0, 500, 500, RED_PIXEL});
+	}
 	return (0);
+}
+
+static int	render_rect(t_data *data, t_rect rect)
+{
+	int	i;
+	int	j;
+
+	if (!data->win_ptr)
+		return (1);
+	i = rect.y;
+	while (i < rect.y + rect.height)
+	{
+		j = rect.x;
+		while (j < rect.x + rect.width)
+			mlx_pixel_put(data->mlx_ptr, data->win_ptr, j++, i, rect.color);
+		i++;
+	}
+	return (0);
+}
+
+static void	render_backgrd(t_data *data, int color)
+{
+	int	i;
+	int	j;
+
+	if (!data->win_ptr)
+		return ;
+	i = 0;
+	while (i < HEIGHT)
+	{
+		j = 0;
+		while (j < WIDTH)
+			mlx_pixel_put(data->mlx_ptr, data->win_ptr, j++, i, color);
+		i++;
+	}
 }
